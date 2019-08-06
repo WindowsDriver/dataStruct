b9d588b621276451ab629fa6e3abc385eb116c30
10160097_lipengxin <10160097li_pengxin@cn.tre-inc.com>
2019-08-06 09:17:04 +0800

    添加链表数据结构

--- dataStruct/dataStruct.h
+++ dataStruct/dataStruct.h
@@ -1,2 +1,15 @@
 #pragma once
 class LinkedList;
+template<typename T>
+class ListNode
+{
+private :
+	int data;
+	ListNode* nextData;
+public:
+	ListNode() :data(0), nextData(0) {}
+	ListNode(T a) :data(a), nextData(0) {}
+	friend class LinkedList;
+};
+
+
